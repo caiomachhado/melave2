3dae65192a7557cbebc380fd0486134d1cff4762
Caio Machado <caioamsul@gmail.com>
2021-08-30 22:47:23 -0300

    Create Account in Progess

--- app/src/main/java/com/example/melave/CreateAccountActivity.kt
+++ app/src/main/java/com/example/melave/CreateAccountActivity.kt
@@ -3,14 +3,21 @@ package com.example.melave
 import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class CreateAccountActivity : AppCompatActivity() {
+@Suppress("DEPRECATION")
+class CreateAccountActivity() : AppCompatActivity() {
 
     private var edit_text_usuario: EditText? = null
     private var edit_text_email: EditText? = null
@@ -45,7 +52,43 @@ class CreateAccountActivity : AppCompatActivity() {
         edit_text_reSenha = findViewById<EditText>(R.id.edit_text_reSenha)
 
         mDatabase = FirebaseDatabase.getInstance()
-        mDatabaseReference = mDatabase?
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        btn_create!!.setOnClickListener( createNewAccount() )
+
     }
 
+    private fun createNewAccount() {
+        usuario = edit_text_usuario?.text.toString()
+        email = edit_text_email?.text.toString()
+        senha = edit_text_senha?.text.toString()
+        reSenha = edit_text_reSenha?.text.toString()
+
+        if (!TextUtils.isEmpty(usuario) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(senha) && !TextUtils.isEmpty(reSenha)){
+            Toast.makeText(this, "Informações preenchidas corretamente", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Entre com mais detalhes", Toast.LENGTH_SHORT).show()
+        }
+
+        mProgressBar!!.setMessage("Registrando Usuario...")
+        mProgressBar!!.show()
+
+        mAuth!!.createUserWithEmailAndPassword(email!!, senha!!).addOnCompleteListener(this){task ->
+            mProgressBar!!.hide()
+
+            if (task.isSuccessful){
+                Log.d(TAG, "CreateUserWithEmail:Sucess")
+
+                val userId = mAuth!!.currentUser!!.uid
+
+                verifyEmail();
+
+                
+
+            }
+
+        }
+
 }
+
