766006d86d0f67ce705e4ba65ff6d3366fe89faf
Caio Machado <caioamsul@gmail.com>
2021-09-01 19:32:49 -0300

    Code Main Activity

--- app/src/main/java/com/example/melave/MainActivity.kt
+++ app/src/main/java/com/example/melave/MainActivity.kt
@@ -7,18 +7,26 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private var btn_login: Button? = null
+    private var btn_newLavador: Button? = null
+    private var btn_newCliente: Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initialise()
-
     }
 
     private fun initialise() {
-        val intent = Intent(this@MainActivity, LoginActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        startActivity(intent)
+
+        btn_login = findViewById(R.id.btn_login)
+        btn_newLavador = findViewById(R.id.btn_newLavador)
+        btn_newCliente = findViewById(R.id.btn_newClient)
+
+        btn_login!!.setOnClickListener { startActivity(Intent(this@MainActivity, LoginActivity::class.java)) }
+        btn_newLavador!!.setOnClickListener { startActivity(Intent(this@MainActivity, CreateAccountActivity::class.java)) }
+        btn_newCliente!!.setOnClickListener { startActivity(Intent(this@MainActivity, CreateAccountActivity::class.java)) }
 
     }
 
